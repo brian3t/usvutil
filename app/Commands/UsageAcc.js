ac134f6086df316214fff3d962f86af7cf4e2378
brian3t <ngxtri@gmail.com>
2020-09-30 21:43:00 -0700

    WIP: sum usage

--- app/Commands/UsageAcc.js
+++ app/Commands/UsageAcc.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const { Command } = require('@adonisjs/ace')
+const Database = use('Database')
+const Usage = use('App/Models/Usage')
 
 class UsageAcc extends Command {
   static get signature () {
@@ -13,7 +15,15 @@ class UsageAcc extends Command {
 
   async handle (args, options) {
     this.info('Dummy implementation for usage:acc command')
-    console.log(`do sth here`)
+    console.log(`accumulating usage table`)
+
+    select sum(count) sumcount, ip,agent, max(created_at), max(updated_at) from `usage`
+    group by ip,agent
+
+
+
+    // Without the following line, the command will not exit!
+    Database.close()
   }
 }
 
